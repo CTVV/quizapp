2d8d9fb10d316369274044693d6543d1919b6028
CTVV <marcin.kedzierski97@gmail.com>
2022-08-22 19:17:56 +0200

    Repaired another bug

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       devServer: {
         static:
           {
-            directory: path.resolve(__dirname, '/'),
+            directory: path.resolve(__dirname, ''),
             publicPath: '/',
           },
         port: 9000,
